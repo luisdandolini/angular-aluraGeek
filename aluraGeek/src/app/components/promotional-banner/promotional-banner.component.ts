8a5718aaf6f3cb92d0cd63393df615a39887c9f8
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-09-28 10:29:59 -0300

    create and style navbar

---
+++ aluraGeek/src/app/components/promotional-banner/promotional-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promotional-banner',
+  templateUrl: './promotional-banner.component.html',
+  styleUrls: ['./promotional-banner.component.css']
+})
+export class PromotionalBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
